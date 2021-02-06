a17c8964b73833780c6043d91cca90abfb37ede4
Vlad Konovalchuk <berlitio600@gmail.com>
2021-02-06 10:26:54 +0200

    Added new app structure; Added controller;

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import PostController from "./posts/posts.controller";
+import App from "./app";
 
-const app = express();
+function loggerMiddleware(
+  request: express.Request,
+  response: express.Response,
+  next
+) {
+  console.log(`${request.method} ${request.path}`);
+  next();
+}
 
-app.get("/", (request, response) => {
-  response.send("Hello world!");
-});
+const app = new App([new PostController()], 5000);
 
-app.listen(5000);
+app.listen();
