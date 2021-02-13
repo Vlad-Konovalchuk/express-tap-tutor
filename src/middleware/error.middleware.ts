35d06077c5fa9604b2b373a2ae3e559ed8c6404b
Vlad Konovalchuk <berlitio600@gmail.com>
2021-02-13 14:13:27 +0200

    Added exceptions, error middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import HttpException from "../exceptions/HttpException";
+
+function errorMiddleware(
+  error: HttpException,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const status = error.status || 500;
+  const message = error.message || "Internal Server Error";
+
+  response.status(status).send(message);
+}
+
+export default errorMiddleware;
